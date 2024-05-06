d2abaf9f49622f4cc328f49447bf11de3c13be4c
Babacarjoop <elouboy269@gmail.com>
2024-05-06 15:52:52 +0200

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,7 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h1>Bienvenue sur mon site Nuxt.js</h1>
+    <p>C'est un exemple de site simple créé avec Nuxt.js.</p>
+  </div>
+</template>
